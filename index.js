114dd0d24c8560688ca5d5ed24a3cd5c8ca87352
Sam <jenki380@msu.edu>
2023-03-20 22:31:37 -0400

    linked supabase to .js file, also added css

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Add JavaScript below
+import { createClient } from "https://cdn.skypack.dev/@supabase/supabase-js@2.8.0"
+
+const supabaseUrl = 'https://ndmepqcoazrhdeaiimjh.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5kbWVwcWNvYXpyaGRlYWlpbWpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzc1NTU0OTksImV4cCI6MTk5MzEzMTQ5OX0.uULbqkCaqecExdYEY_b_jCgMxvyyJrGQQ5mGFzcFVOk'
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+
+async function getBooks() {
+  let { data: books, error } = await supabase
+  .from('books')
+  .select('*')
+
+  for (let book of books) {
+    let bookList = document.getElementById('books'); bookList.innerHTML += `<tr><td>${book.title}</td><td>${book.author}</td><td>${book.description}</td></tr>`;
+  }
+  
+}
+
+getBooks();
